b342337ba0c4604ba508580d91e5bfbe563ff036
yijiejiang <yijiejiang@tencent.com>
2024-02-21 17:18:54 +0800

    go

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,16 @@
 // @ts-ignore
 import React from 'react';
-import ReactSequenceFramePlayer from './ReactSequenceFramePlayer';
+import ReactSequenceFramePlayer, {
+  IReactSequenceFramePlayerProps,
+  IReactSequenceFramePlayerRef,
+} from './ReactSequenceFramePlayer';
 import useReactSequenceFramePlayer, {
   IReactSequenceFramePlayerHookProps,
 } from './use-react-sequence-frame-player';
 
 export { ReactSequenceFramePlayer, useReactSequenceFramePlayer };
-export type { IReactSequenceFramePlayerHookProps };
+export type {
+  IReactSequenceFramePlayerHookProps,
+  IReactSequenceFramePlayerProps,
+  IReactSequenceFramePlayerRef,
+};
