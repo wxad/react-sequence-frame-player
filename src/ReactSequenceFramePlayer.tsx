594b5856113bdccc4bdc299788af44dceaab4c08
yijiejiang <yijiejiang@tencent.com>
2024-08-12 17:08:04 +0800

    chore: format

--- src/ReactSequenceFramePlayer.tsx
+++ src/ReactSequenceFramePlayer.tsx
@@ -1,50 +1,50 @@
 // @ts-ignore
 import React from 'react';
-import { forwardRef, useImperativeHandle, useRef } from "react"
+import { forwardRef, useImperativeHandle, useRef } from 'react';
 import useReactSequenceFramePlayer, {
   IReactSequenceFramePlayerHookProps,
-} from "./use-react-sequence-frame-player"
+} from './use-react-sequence-frame-player';
 
 export interface IReactSequenceFramePlayerProps
   extends IReactSequenceFramePlayerHookProps {
   /**
    * 容器宽度
    */
-  width?: number | string
-  height?: number | string
+  width?: number | string;
+  height?: number | string;
 }
 
 export interface IReactSequenceFramePlayerRef {
-  root: HTMLDivElement | null
-  play: () => void
-  pause: () => void
+  root: HTMLDivElement | null;
+  play: () => void;
+  pause: () => void;
 }
 
-const ReactSequenceFramePlayer: React.ForwardRefExoticComponent<
-  IReactSequenceFramePlayerProps &
-    React.RefAttributes<IReactSequenceFramePlayerRef>
-> = forwardRef(({ width, height, ...otherProps }, ref) => {
-  const rootRef = useRef<HTMLDivElement | null>(null)
-  const { dom, play, pause } = useReactSequenceFramePlayer(otherProps)
+const ReactSequenceFramePlayer: React.ForwardRefExoticComponent<IReactSequenceFramePlayerProps &
+  React.RefAttributes<IReactSequenceFramePlayerRef>> = forwardRef(
+  ({ width, height, ...otherProps }, ref) => {
+    const rootRef = useRef<HTMLDivElement | null>(null);
+    const { dom, play, pause } = useReactSequenceFramePlayer(otherProps);
 
-  useImperativeHandle(ref, () => ({
-    root: rootRef.current,
-    play,
-    pause,
-  }))
+    useImperativeHandle(ref, () => ({
+      root: rootRef.current,
+      play,
+      pause,
+    }));
 
-  return (
-    <div
-      ref={rootRef}
-      style={{
-        position: "relative",
-        width,
-        height,
-      }}
-    >
-      {dom}
-    </div>
-  )
-})
+    return (
+      <div
+        ref={rootRef}
+        style={{
+          position: 'relative',
+          width,
+          height,
+        }}
+      >
+        {dom}
+      </div>
+    );
+  }
+);
 
-export default ReactSequenceFramePlayer
+export default ReactSequenceFramePlayer;
